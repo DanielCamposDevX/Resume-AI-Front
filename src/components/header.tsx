e3991aa621eaae429013b40522e0859000e575e5
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-12-05 10:02:45 -0300

    update/ Design Login page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -28,7 +28,7 @@ export function Header(props: Userdataty) {
     }
 
     return (
-        <div className="px-6 py-3 flex items-center justify-between border-b z-50">
+        <div className="px-6 py-3 flex items-center justify-between border-b border-green-400/60 z-50">
             <div className="flex">
                 <Avatar className="w-7 h-7 mr-2">
                     <AvatarImage src={IconMain} />
--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -23,19 +23,19 @@ export function Login() {
 
 
     return (
-        <div className="min-h-screen flex flex-col">
+        <div className="min-h-screen flex flex-col gradient">
             <Header userdata={null} setUserdata={1} />
             <main className="fixed w-full h-screen flex justify-center items-center">
-                <Card className="w-[350px] flex flex-col justify-center items-center">
+                <Card className="w-[350px] flex flex-col justify-center items-center border border-green-400/60 ">
                     <CardHeader className="mb-5 flex justify-center items-center">
                         <CardTitle className="text-accent-foreground text-xl">Login</CardTitle>
                     </CardHeader>
                     <CardContent className="flex flex-col justify-center items-center gap-3">
                         <div className="flex flex-col gap-3">
                             <label>Email</label>
-                            <Input type="email" required placeholder="Email" id="email" value={user.email} onChange={e => setUser({ ...user, email: e.target.value })} />
+                            <Input type="email" className="border border-green-400/30" required placeholder="Email" id="email" value={user.email} onChange={e => setUser({ ...user, email: e.target.value })} />
                             <label>Senha</label>
-                            <Input type="password" required placeholder="Senha" id="pass" value={user.pass} onChange={e => setUser({ ...user, pass: e.target.value })} />
+                            <Input type="password" className="border border-green-400/30" required placeholder="Senha" id="pass" value={user.pass} onChange={e => setUser({ ...user, pass: e.target.value })} />
                         </div>
                     </CardContent>
                     <CardFooter className="flex flex-col gap-3 justify-between mt-10">
